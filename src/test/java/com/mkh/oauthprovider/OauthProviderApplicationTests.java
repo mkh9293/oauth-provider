b21ccc23cd375f5069e436251cfc95292f9a4907
문광훈 <gwanghun.moon@hancom.com>
2019-03-14 12:07:26 +0900

    init setting

---
+++ src/test/java/com/mkh/oauthprovider/OauthProviderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mkh.oauthprovider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OauthProviderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
