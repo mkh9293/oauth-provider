2b59e85685c4c755d9006cc4626a1ee58da4b2b0
문광훈 <gwanghun.moon@hancom.com>
2019-03-15 18:02:49 +0900

    create oauth-developers module
    developer setting web site

---
+++ oauth-developers/src/main/java/com/mkh/oauthdevelopers/OauthDevelopersApplication.java
@@ -0,0 +1,13 @@
+package com.mkh.oauthdevelopers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthDevelopersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthDevelopersApplication.class, args);
+    }
+
+}
