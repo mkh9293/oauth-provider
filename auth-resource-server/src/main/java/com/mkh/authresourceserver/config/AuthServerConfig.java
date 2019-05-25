71f31e54f1b37a8801fc3359cb737d4ac70229f0
GwangHun Moon <mkh9293@gmail.com>
2019-05-25 23:59:59 +0900

    Update token type
    jwt 토큰으로 방식 변경

--- auth-resource-server/src/main/java/com/mkh/authresourceserver/config/AuthServerConfig.java
+++ auth-resource-server/src/main/java/com/mkh/authresourceserver/config/AuthServerConfig.java
@@ -1,8 +1,53 @@
 package com.mkh.authresourceserver.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthServerConfig {
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+    @Bean
+    public JwtAccessTokenConverter tokenConverter() {
+        JwtAccessTokenConverter conveter = new JwtAccessTokenConverter();
+        return conveter;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(tokenConverter());
+    }
+
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .tokenStore(tokenStore())
+                 .accessTokenConverter(tokenConverter());
+    }
+
+//    @Override
+//    public void configure(ClientDetailsServiceConfigurer config) throws Exception {
+//        config.inMemory().withClient("client").secret("secret").authorizedGrantTypes("client_credentials")
+//                .scopes("read").authorities("USER");
+//    }
+
+    public void configure(AuthorizationServerSecurityConfigurer oauth) throws Exception {
+        oauth.tokenKeyAccess("permitAll()").checkTokenAccess("permitAll()");
+    }
 }
--- auth-resource-server/src/main/java/com/mkh/authresourceserver/config/ResourceServerConfig.java
+++ auth-resource-server/src/main/java/com/mkh/authresourceserver/config/ResourceServerConfig.java
@@ -1,6 +1,9 @@
 package com.mkh.authresourceserver.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
 @EnableResourceServer
